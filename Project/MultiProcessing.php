bf5753d0b0ac2d92d2af243cda81c9cc75c8d411
Morody <adel-ufa@mail.ru>
2024-06-03 10:28:15 +0300

    'realize'

--- Project/MultiProcessing.php
+++ Project/MultiProcessing.php
@@ -1,61 +1,129 @@
 <?php
+require_once 'C:\Users\User\Desktop\Multi\vendor\autoload.php';
 
-namespace Project;
 
 use Symfony\Component\Process\Process;
+use Project\Bakery;
 
 class MultiProcessing
 {
-    /**
-     * @var array
-     */
     private array $pullProcess;
 
+    public int $num_proc = 3; # кол-во процессов
+    public int $iter_per_proc = 10; # итераций в процессе
+
+    /*
+        определяем индентификаторы для shmop, чтобы иметь к ним доступ для чтения и записи в распред. память
+    */
+    public int $id_shmop_number = 5; 
+    public int $id_shmop_choosing = 6;
+    public int $id_shmop_state = 7;
+    public int $id_shmop_counter = 8;
+    
 
 
     public function __construct()
     {
+        shmop_open($this->id_shmop_counter, 'c', 0644, 5);    // инициализируем блок в распределенной памяти для каждого хранилища
+        shmop_open($this->id_shmop_number, "c", 0644, 50);    // для счетчика - 5 байт
+        shmop_open($this->id_shmop_choosing, "c", 0644, 50);  // для массивов - 50 байт
+        shmop_open($this->id_shmop_state, "c", 0644, 50);     //
 
-        for ($i = 0; $i < 5; $i++)
-        {
-            $this->initProcess(__DIR__ . '/bakery.php');
+        /*заполняем массивы начальными значениями
+        * размер = кол-во процессов
+        */
+
+        $number = array_fill(0, $this->num_proc, 0);
+        $choosing = array_fill(0, $this->num_proc, 'false'); 
+        $state = array_fill(0, $this->num_proc, '_');
+        
+
+        /* записываем начальное значение для счетчика количества входа процессов в критическую секцию
+        */
+        shmop_write(shmop_open($this->id_shmop_counter, 'w', 0644, 5), 0, 0);    
+        
+        //записываем ранее созданные массивы в виде строки в распред. память
+        shmop_write(shmop_open($this->id_shmop_number, "w", 0644, 50), implode(',', $number), 0);
+        shmop_write(shmop_open($this->id_shmop_choosing, "w", 0644, 50), implode(',', $choosing), 0);   
+        shmop_write(shmop_open($this->id_shmop_state, "w", 0644, 50), implode(',', $state), 0);
+
+
+        $scripts = [
+            __DIR__ . "/bakery.php",
+            __DIR__ . "/bakery.php",
+            __DIR__ . "/bakery.php"]; # три процесса
+        
+        $n = 0;
+        foreach($scripts as $script){
+            $this->initProcess($script, $n); // инициализируем три процесса
+            $n++;
         }
-    
 
         while(true)
         {
             sleep(5);
 
-            $this->checkFinish();
+            $this->checkFinish(); // проверяем завершенность процессов
         }
 
     }
 
-    private function initProcess($script): Process
+    private function initProcess($script, $num_proc): Process
     {
-        $process = new Process(['php', $script]);
-        $process->start();
+        /* инициализируем процесс
+        * передаем в процесс исполняемый скрипт и переменные окружения через $_SERVER
+        *
+        */
+        $process = new Process(['php', $script], null, ['num_proc' => $num_proc, 'id_shmop_number' => $this->id_shmop_number,
+                                                        'id_shmop_choosing' => $this->id_shmop_choosing, 'id_shmop_state' => $this->id_shmop_state,
+                                                        'id_shmop_counter' => $this->id_shmop_counter]);
+        $process->start(); # запускаем процессы асинхронно
 
-        $this->pullProcess[$process->getPid() . ':' . $script] = $process;
+        $this->pullProcess[$process->getPid()] = $process; // помещаем процессы в пул процессов
         return $process;
     }
 
     private function checkFinish(): void
     {
-        if(empty($this->pullProcess))
+        if(empty($this->pullProcess)) // Проверяем завершенность всех процессов
         {
+            /*
+            * закрываем блоки в распред. памяти
+            * в Win 10 с этим проблемы
+            */
+            if(shmop_delete(shmop_open($this->id_shmop_number, 'w', 0644, 50))){
+                echo "Shmop number: delete" . PHP_EOL;
+            }
+            if(shmop_delete(shmop_open($this->id_shmop_choosing, 'w', 0644, 50))){
+                echo "Shmop choosing: delete". PHP_EOL;
+            }
+            if(shmop_delete(shmop_open($this->id_shmop_state, 'w', 0644, 50))){
+                echo "Shmop state: delete" . PHP_EOL;
+            }
+            if(shmop_delete(shmop_open($this->id_shmop_counter, 'w', 0644, 5))){
+                echo "Shmop counter: delete" . PHP_EOL;
+            }
+
+            $id_read_counter = shmop_open($this->id_shmop_counter, 'w', 0644, 5);
+
+            echo "Final counter value: " . intval(rtrim(shmop_read($id_read_counter, 0, 5), "\0 ")) . PHP_EOL;
+
+            // проверка конечного значения 'счетчика процесса' * 'итераций в процессе'
+            assert(intval(rtrim(shmop_read($id_read_counter, 0, 5), "\0 ")) == $this->num_proc * $this->iter_per_proc, "Final counter value does not match expected");
             return;
         }
-
         foreach($this->pullProcess as $nameProcess => $process){
             if ($process instanceof Process && $process -> isTerminated())
             {
-                echo '#FINISHED: ' . $nameProcess . ' ' . $process ->getOutput() . PHP_EOL;
+                echo '#FINISHED: ' . $nameProcess . ' ' . $process->getOutput() . PHP_EOL; // вывод содержимого каждого из процессов
                 unset($this->pullProcess[$nameProcess]);
             }
         }
+
     }
 
 }
+
+new MultiProcessing(); // запуск
 
 ?>
--- Project/bakery.php
+++ Project/bakery.php
@@ -1,14 +1,149 @@
 <?php
 
-$size = 128;
-$string = 'something';
+namespace Project;
 
-$shm = shmop_open(10, "c", 0644, 100);
-shmop_write($shm, 'some', 0);
+require_once 'C:\Users\User\Desktop\Multi\vendor\autoload.php';
+class Bakery{
 
-$shm_data = shmop_read($shm, 0, 10);
+    public int $number_id_shmop;
+    public int $choosing_id_shmop;
+    public int $state_id_shmop;
 
-echo $shm_data;
-//class Bakery{
-//}
+    public function __construct($number_id_shmop, $choosing_id_shmop, $state_id_shmop)
+    {
+        $this->number_id_shmop = $number_id_shmop;
+        $this->choosing_id_shmop = $choosing_id_shmop;
+        $this->state_id_shmop = $state_id_shmop;
+    }
+
+    /*
+    * обновление данных в памяти
+    */
+    public function updateValFromMem(int $process_id, string $val, int $shm_id): void
+    {
+        $outFromMem = $this->readFromMem($shm_id);
+        $len_str = strlen(implode(",", $outFromMem));
+        $outFromMem[$process_id] = $val;
+        shmop_write(shmop_open($shm_id, 'w', 0644, 50), str_pad(implode(",", $outFromMem), 50 - $len_str), 0);
+    }
+
+    # считывание данных из память
+    public function readFromMem(int $shm_id)
+    {
+        return explode(",", rtrim(shmop_read(shmop_open($shm_id, 'w', 0644, 50), 0, 50), "\0 "));
+    }
+
+    /*
+    * функция для сравнения кортежа как в Python ((a1, b1) < (a2, b2) | a1 < a2 or a1 == a2 and b1 < b2) 
+    */
+    public function compare($num_one, $one, $num_two, $two): bool
+    {
+        if ($num_one < $num_two){
+            return true;
+        } else if ($num_one > $num_two){
+            return false;
+        } else {
+            if ($one < $two){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    /*
+    *
+    * здесь определяется взаимоисключение процессов и определяется какой процесс отправится в критическую секцию
+    */
+    public function lock(int $process_id)
+    {
+        // начало doorway
+        $this->updateValFromMem($process_id, 'R', $this->state_id_shmop);
+        $this->updateValFromMem($process_id, 'true', $this->choosing_id_shmop);
+        sleep(rand(1, 10)); // симуляция атомарности операции записи
+        $this->updateValFromMem($process_id, intval(max($this->readFromMem($this->number_id_shmop))) + 1, $this->number_id_shmop);
+        sleep(rand(1, 10)); // симуляция атомарности операции чтения
+        $this->updateValFromMem($process_id, 'false', $this->choosing_id_shmop);
+        // конец doorway
+
+        /* пока все процессы не будут иметь значения в массиве 'choosing' - false
+        * т.е. все процессы ждут пока каждый из них не выйдут из doorway
+        * процессы, которые имеют значения в массиве 'number' - 0 и не имеют наименьшего значения, ожидают своей очереди
+        * 
+        */
+        for ($i = 0; $i < 3; $i++){
+            while(filter_var($this->readFromMem($this->choosing_id_shmop)[$i], FILTER_VALIDATE_BOOLEAN)){}
+            while(intval($this->readFromMem($this->number_id_shmop)[$i]) !== 0
+                    && $this->compare(intval($this->readFromMem($this->number_id_shmop)[$i]), $i, intval($this->readFromMem($this->number_id_shmop)[$process_id]), $process_id)){}
+        }
+
+        $this->updateValFromMem($process_id, 'C', $this->state_id_shmop); // процесс находится в критической секции
+
+    }
+
+    // процесс выходит из критической секции, состояние 'choosing' и значение 'number' обнуляются
+    public function unlock(int $process_id)
+    {
+        $this->updateValFromMem($process_id, '_', $this->state_id_shmop);
+        $this->updateValFromMem($process_id, 0, $this->number_id_shmop);
+    }
+    
+
+}
+
+// симуляция действий процесса в критической секции
+function criticalSection(int $shm_counter, int $process_id)
+    {
+        $id_read_counter = shmop_open($shm_counter, 'w', 0644, 5);
+
+        $shared_counter = intval(rtrim(shmop_read($id_read_counter, 0, 5), "\0 ")); // читается счетчик из распр. памяти
+        $len_str = strlen(rtrim(shmop_read($id_read_counter, 0, 5), "\0 ")); // вычисляется его длина строки перед изменением
+
+        $old_value = $shared_counter;
+        $new_value = $old_value + 1; // увеличиваем счетчик 
+        $shared_counter = $new_value;
+
+        shmop_write($id_read_counter, str_pad(strval($shared_counter), 5 - $len_str), 0); // записываем обновленный счетчик в распр. память
+        
+        echo "Process {$process_id} is working..." . PHP_EOL;       // симулируем работу процесса
+        sleep(rand(0, 10));                                         //     
+        echo "Process {$process_id} finished working!" . PHP_EOL;   //         
+
+        assert($shared_counter == $new_value, "Mutual exclusion violated"); // проверка условия взаимоисключения процессов из крит. секции 
+    }
+
+# вывод состояния всех процессов на данный момент
+function generateASCII(Bakery $lock): string
+    {
+        $state_symbols = ['R' => 'Requesting', 'C' => 'In Critical Section', '_' => 'Waiting'];
+        $ascii_art = PHP_EOL . 'System State: ' . PHP_EOL;
+        for ($i = 0; $i < 3; $i++){
+            $ascii_art .= "Process {$i}: {$state_symbols[$lock->readFromMem($_SERVER['id_shmop_state'])[$i]]}" . PHP_EOL;
+        }
+        return $ascii_art;
+    }
+
+# точка распределения задачи на процессы
+function processFunction(Bakery $lock, int $shm_counter, int $process_id)
+    {
+        for ($i = 0; $i < 5; $i++){ # 5 итераций на каждый процесс
+            $lock->lock($process_id); # определяет какой процесс попадет в критическую секцию, блокировка остальных процессов
+            echo generateASCII($lock) . PHP_EOL; # вывод состояние процессов на данный момент
+            criticalSection($shm_counter, $process_id); # после выхода из блокировки процесс получает возможность попасть в крит. секцию
+            $lock->unlock($process_id); # после выхода из крит. секции значения 'choosing' и 'number' обнуляется
+            echo generateASCII($lock) . PHP_EOL;  # вывод состояние процессов на данный момент
+            sleep(1); # пауза между итерациями
+        }
+    }
+
+/*
+*
+* Создаем объект lock с id в shmop с общими блоками хранения в распред. памяти
+* инициализируем алгоритм функцией processFunction с счетчиком количества входа процессов в критическую секцию и количеством процессов
+* выводим состояние системы после заврешения каждого из процессов
+*/
+$lock = new Bakery($_SERVER['id_shmop_number'], $_SERVER['id_shmop_choosing'], $_SERVER['id_shmop_state']); 
+processFunction($lock, $_SERVER['id_shmop_counter'], $_SERVER['num_proc']);
+
+echo generateASCII($lock) . PHP_EOL;
+
 ?>
